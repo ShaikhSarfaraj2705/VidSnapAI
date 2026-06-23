30adf63a76f9cd4d585221e00adaf5c982fea4a8
ShaikhSarfaraj2705 <shaikhsarafaraj62@gmail.com>
2026-06-23 16:42:31 +0530

    Updated VidSnapAI project

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request
+import uuid
+from werkzeug.utils import secure_filename
+import os
+from text_to_audio import text_to_speech_file
+from generate_process import create_reel
+
+UPLOAD_FOLDER = 'user_uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ 
+
+@app.route("/")
+def home():
+    reels_folder = "static/reels"
+
+    reels = []
+
+    if os.path.exists(reels_folder):
+        reels = sorted(os.listdir(reels_folder), reverse=True)[:3]
+
+    return render_template("index.html", reels=reels)
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    myid = uuid.uuid1()
+    if request.method == "POST":
+        print(request.files.keys())
+        rec_id = request.form.get("uuid")
+        desc = request.form.get("text")
+        input_files = []
+        for key, value in request.files.items():
+            print(key, value)
+            # Upload the file
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                if(not(os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], rec_id)))):
+                    os.mkdir(os.path.join(app.config['UPLOAD_FOLDER'], rec_id))
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], rec_id,  filename))
+                input_files.append(file.filename)
+                print(file.filename)
+            # Capture the description and save it to a file
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], rec_id, "desc.txt"), "w") as f:
+                f.write(desc)
+        for fl in input_files:
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], rec_id,  "input.txt"), "a") as f:
+                f.write(f"file '{fl}'\nduration 1\n")
+        text_to_speech_file(desc, rec_id)
+        create_reel(rec_id)
+
+
+    return render_template("create.html", myid=myid)
+
+@app.route("/gallery")
+def gallery():
+    reels = os.listdir("static/reels")
+    print(reels)
+    return render_template("gallery.html", reels=reels)
+
+app.run(debug=True)
